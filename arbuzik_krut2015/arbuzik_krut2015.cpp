d423696553955f272d2db8459bf4d6ed5f943206
Top <Top@DESKTOP-G8DVQP6>
2024-12-09 16:50:56 +0500

    Добавьте файлы проекта.

---
+++ arbuzik_krut2015/arbuzik_krut2015.cpp
@@ -0,0 +1,66 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	double num1 , num2;
+	
+	std::cout << "введите первое число";
+	std::cin >> num1 ;
+	std::cout << "введите второе число";
+	std::cin >> num2;
+
+	std::cout << "сумма" << num1 + num2 << "\n";
+	std::cout << "разность" << num1  - num2 << "\n";
+	std::cout << "произведение" << num1  * num2 << "\n";
+	std::cout << "chasnost" << num1 / num2 << "\n"; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
